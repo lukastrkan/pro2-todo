748e92c89794125c118548a9fefd1899c1dcd88e
lukastrkan <lukas@trkan.cz>
2022-09-29 09:43:05 +0200

    Main class

---
+++ src/main/java/cz/uhk/pro2/todo/TodoMain.java
@@ -0,0 +1,11 @@
+package cz.uhk.pro2.todo;
+
+import javax.swing.*;
+
+public class TodoMain {
+
+    public static void main(String[] args) {
+        String title = JOptionPane.showInputDialog("Ender task name:");
+        //TODO: create 2 tasks and print them
+    }
+}
