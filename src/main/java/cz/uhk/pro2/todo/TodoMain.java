c5ce9aa5a7f31a167a83c038ffeeb48bbbf681b9
Lukáš Trkan <lukas@trkan.cz>
2022-10-20 08:48:42 +0200

    window

--- src/main/java/cz/uhk/pro2/todo/TodoMain.java
+++ src/main/java/cz/uhk/pro2/todo/TodoMain.java
@@ -4,29 +4,60 @@ import cz.uhk.pro2.todo.model.Task;
 import cz.uhk.pro2.todo.model.TaskList;
 
 import javax.swing.*;
+import java.awt.*;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
-public class TodoMain {
+public class TodoMain extends JFrame {
+
+    private TaskList taskList = new TaskList();
+
+    private JTextArea txtOutput = new JTextArea(20,40);
+
+
+    public TodoMain() {
+        setTitle("ToDo list");
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(500, 500);
+
+        JPanel panel = new JPanel();
+        panel.setBackground(Color.CYAN);
+        this.add(panel, BorderLayout.NORTH);
+        this.add(new JScrollPane(txtOutput), BorderLayout.SOUTH);
+        JButton btnAdd = new JButton("Přidat úkol");
+        panel.add(btnAdd);
+        btnAdd.addActionListener(e -> addTask(taskList));
+
+        txtOutput.setText(taskList.getTasks().toString());
+
+        pack();
+    }
+
+    private void addTask(TaskList taskList) {
+        // Kontrola správnosti dat
+        try {
+            // Zadání od uživatele
+            String description = JOptionPane.showInputDialog(null, "Zadejte popis úkolu:").trim();
+            if (description.isEmpty())
+                return;
+            LocalDate date = LocalDate.parse(JOptionPane.showInputDialog(null, "Zadejte datum ve tvaru: dd/MM/yyyy").trim(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+
+            // Vytvoření úkolu a přidání úkolu do tasklistu
+            taskList.addTask(new Task(date, description));
+            txtOutput.setText(taskList.getTasks().toString());
+        } catch (DateTimeParseException ex) {
+            JOptionPane.showMessageDialog(null, "Nesprávně zadané datum.", "Upozornění", JOptionPane.ERROR_MESSAGE);
+        } catch (NullPointerException ex) {
+            JOptionPane.showMessageDialog(null, "Nezadali jste žádnou hodnotu! \n\n" + ex.getMessage(), "Upozornění", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
     public static void main(String[] args) {
-        var taskList = new TaskList();
-
-        // create tasks until user enters empty name
-        while (true) {
-            String description = JOptionPane.showInputDialog("Enter task name");
-            if (description == null || description.isEmpty()) {
-                break;
-            }
-
-            String date = JOptionPane.showInputDialog("Enter date (yyyy-MM-dd)");
-
-            Task task = new Task(LocalDate.parse(date), description);
-            taskList.addTask(task);
-        }
-
-        //print all tasks
-        for (Task task : taskList.getTasks()) {
-            System.out.println(task);
-        }
+        SwingUtilities.invokeLater(() -> {
+            TodoMain tm = new TodoMain();
+        });
 
     }
 }
